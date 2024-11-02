e91e07220141b0b455209614c19ed48a44285429
nikolay-gg <nikolay.gg99@gmail.com>
2024-11-02 19:40:51 +0200

    feat(qol): add escape with double j

--- lua/ngeorgiev/remap.lua
+++ lua/ngeorgiev/remap.lua
@@ -37,3 +37,5 @@ vmap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
 -- Easy find and replace
 vmap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Escape with jj
+vmap.set("i", "jj", "<Esc>")
